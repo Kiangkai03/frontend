727f72e7e8cb9f690c538cc3ba989e9348b75d09
Kiangkai03 <mistergood0071@gmail.com>
2021-08-17 13:38:18 +0700

    t

--- components/layouts/admin.header.js
+++ components/layouts/admin.header.js
@@ -14,7 +14,7 @@ const AdminHeader = () =>{
             </a>
           </li>
           <li className="nav-item d-none d-sm-inline-block">
-            <a href="index3.html" className="nav-link">
+            <a href="#" className="nav-link">
               Home
             </a>
           </li>
