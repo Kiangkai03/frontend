c0ac88740d452edf3f4de65ffa7a62c9c2f8f4a3
Kiangkai03 <mistergood0071@gmail.com>
2021-11-05 12:16:50 +0700

    12

--- components/layouts/admin.sidebar.js
+++ components/layouts/admin.sidebar.js
@@ -1,14 +1,14 @@
 import Link from 'next/link';
+import Image from 'next/image'
+import React from 'react'
 
 const AdminSidebar = () => {
     return (
-        <>
-{/* Main Sidebar Container */}
 <aside className="main-sidebar sidebar-dark-primary elevation-4">
   {/* Brand Logo */}
   <Link href='#'>
   <a className="brand-link">
-    <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{opacity: '.8'}} />
+    <Image src="/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{opacity: '.8'}} width={20} height={20} />
     <span className="brand-text font-weight-light">Admin Panel</span>
   </a>
   </Link>
@@ -17,7 +17,7 @@ const AdminSidebar = () => {
     {/* Sidebar user panel (optional) */}
     <div className="user-panel mt-3 pb-3 mb-3 d-flex">
       <div className="image">
-        <img src="dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
+        <Image src="/dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" width={50} height={50} />
       </div>
       <div className="info">
       <Link href='#'>
@@ -52,48 +52,32 @@ const AdminSidebar = () => {
         </Link>
         </li>
         <li className="nav-item">
-          <Link href='/users'>
-              <a className="nav-link">
-                <i className="nav-icon fas fa-th" />
-                <p>
-                  จัดการสมาชิก
-                  <span className="right badge badge-danger">New</span>
-                </p>
-              </a>
-            </Link>
+        <Link href='/users'>
+          <a className="nav-link">
+            <i className="nav-icon fas fa-th" />
+            <p>
+              จัดการสมาชิก
+              <span className="right badge badge-danger">New</span>
+            </p>
+          </a>
+          </Link>
         </li>
         <li className="nav-item">
         <Link href='#'>
           <a className="nav-link">
-            <i className="nav-icon fas fa-copy" />
+            <i className="nav-icon fas fa-copy text-red" />
             <p>
               ออกจากระบบ
-              <i className="fas fa-angle-left right" />
             </p>
           </a>
         </Link>
-          <ul className="nav nav-treeview">
-            <li className="nav-item">
-            <Link href='#'>
-              <a className="nav-link">
-                <i className="far fa-circle nav-icon" />
-                <p>Logout</p>
-              </a>
-            </Link>
-            </li>
-            </ul>
           </li>
           </ul>
     </nav>
     {/* /.sidebar-menu */}
   </div>
   {/* /.sidebar */}
-
-
 </aside>
-
-
-        </>
     )
 }
 
